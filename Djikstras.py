74d4527a19803526abd17022c66a6edb586d4219
Rajiv Narayan <rajiv.narayan12@gmail.com>
2019-12-28 09:28:07 +1100

    Added in map bounds verification function

--- Djikstras.py
+++ Djikstras.py
@@ -15,35 +15,46 @@ MAX_Y = 0
 class Graph:
     #Define the dimensions of the graph
     def __init__(self, dim_x, dim_y):
-        self.rows, self.cols = (dim_x, dim_y)
+        self.cols, self.rows = (dim_x, dim_y)
         global MAX_X, MAX_Y
         MAX_X = dim_x
         MAX_Y = dim_y
-        #Defining a 2D array
-        self.graph = [['*' for i in range(self.rows)] for j in range(self.cols)]
+        #Defining a 2D array with distances set as -1 (for infinitely long)
+        #All adjascent coordinates are length 1 away
+        self.graph = [[-1 for x in range(self.cols)] for y in range(self.rows)]
 
     def printGraph(self):
         for item in self.graph:
             print(item)
 
     def setPointAs(self, x_coord, y_coord, val):
-        self.graph[x_coord][y_coord] = val
+        self.graph[y_coord][x_coord] = val
 
+    def withinBounds(self, x_coord, y_coord):
+        return (x_coord >= MIN and x_coord < MAX_X) and (y_coord >= MIN and y_coord < MAX_Y)
     #traverse a path diven the starting and ending position in the graph
     def traverse(self, x_start, y_start, x_end, y_end):
-        for coord in [x_start, x_end]:
-            if (coord < MIN or coord > MAX_X):
-                raise ArithmeticError("Entered x coordinate exceeds map limits")
-        for coord in [y_start, y_end]:
-            if (coord < MIN or coord > MAX_Y):
-                raise ArithmeticError("Entered y coordinate exceeds map limits")
+        if (not self.withinBounds(x_start, y_start)):
+            raise ArithmeticError("Entered start coordinates exceeds map limits")
+        if (not self.withinBounds(x_end, y_end)):
+            raise ArithmeticError("Entered end coordinates exceeds map limits")
         #Make the map look more intuitive as to where the start and end are
-        self.setPointAs(x_start, y_start, "S")
-        self.setPointAs(x_end, y_end, "E")
+        toSearch = [[(x, y) for x in range(0, MAX_X)] for y in range(0, MAX_Y)]
+        prev     = [[(-1, -1) for x in range(0, MAX_X)] for y in range(0, MAX_Y)]
+        self.setPointAs(x_start, y_start, 0) #Set distance to 0
+        #Setup the queue -> Based on the position of the graph
+        currQ   = set()
+        currQ.add((x_start, y_start))
+        while len(currQ) > 0:
+            curPos = currQ.pop()
+
+
+
+#Traversal algorithm
+# Set of coordinates that represent the position not yet visited
+
+
 
 if __name__ == "__main__":
-    graph = Graph(18, 18)
-    graph.printGraph()
-    graph.traverse(2, 2, 5, 5)
-    print("\n\n\n")
-    graph.printGraph()
+    graph = Graph(16, 8)
+    graph.traverse(2, 7, 5, 5)
